1e97a5ed37a69ea79d3b4ecf0d71f3804b5faaaf
vishnuprasath03 <eeevishnuprasath@gmail.com>
2024-07-09 14:44:49 +0530

    arrow gunction

---
+++ d3func/arrowfunc/prmarr.js
@@ -0,0 +1,13 @@
+let arr=[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
+let prime=(arr)=>{
+let num=arr.map(el=>{
+ if(el==1){
+    return false;
+ }
+ if(el<=3){
+    num.push;
+ }
+
+
+})}
+prime(arr)
---
+++ d3func/arrowfunc/sumarr.js
@@ -0,0 +1,9 @@
+
+let aray=()=>{
+    let arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+    let total=0;
+    arr.map(num=>{
+     total+=num;})
+console.log(total);
+}
+aray()
