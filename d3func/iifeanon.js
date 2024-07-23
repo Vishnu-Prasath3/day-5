6801ca76d54c5c513dceb9e653335bda9229e914
vishnuprasath03 <eeevishnuprasath@gmail.com>
2024-07-23 16:22:20 +0530

    day-11 task added

--- d3func/iifeanon.js
+++ d3func/iifeanon.js
@@ -51,8 +51,8 @@ const isPrime = num => {
     return num > 1;
 };
 
-const primeNumbers = function(arr) {
-    return arr.filter(isPrime);
+const primeNumbers = function(z) {
+    return z.filter(isPrime);
 };
 console.log(primeNumbers([1, 2, 3, 4, 5, 6, 7])); // Output: [2, 3, 5, 7]
 
---
+++ day-11/prblm.js
@@ -0,0 +1,8 @@
+// let n=['5']
+// let numers=userinput[1].map(num=>Number(num))
+// console.log(numers)
+
+var a=20;
+let b=20;
+let f = a!= b
+console.log(f);
