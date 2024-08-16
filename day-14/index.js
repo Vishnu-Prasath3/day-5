03447b6236a5691071a15d2633aa49fd15d27de1
VishnuPrasath <eeevishnuprasath@gmail.com>
2024-08-16 08:24:45 +0530

    edited

--- day-14/index.js
+++ day-14/index.js
@@ -1,21 +1,18 @@
 console.log("LIVE")
-let url="https://gist.githubusercontent.com/rvsp/add40254aa126f045837fa5b51f47f1f/raw/4d724bfabf4cce7379a386e23bef6576ab99a2f9/pagination.json"
+
+let url="https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json"
 async function load(link){
     let response=await fetch(link)
-    let data=response.json()
-    console.log(data);
+    let data=await response.json()
+    let id=data.map(a=>a.id);
+    let name=data.map(a=>a.name);
+    return id+name;
 }
-load(url)
+let founData=load(url).then(element=>element)
+
 let el1=document.getElementById("root")
 let fdiv=document.createElement("div")
 el1.appendChild(fdiv)
-//creating an seconnd div
-let sdiv=document.createElement("div")
-sdiv.setAttribute("Id","prasathly")
-el1.appendChild(sdiv);
-let el2=document.createElement("div");
-sdiv.appendChild(el2)
 
-let i=new Array();
 
 
