35445302043b6bc421acd9c9cfd388838e363c8c
VishnuPrasath <eeevishnuprasath@gmail.com>
2024-08-23 22:01:08 +0530

    input value not bought

--- api-task/task-1/index.js
+++ api-task/task-1/index.js
@@ -1,13 +1,15 @@
-let url,number,details; //api url
+let url,details; //api url
 let root=document.getElementById("root").innerText="enter an mobile number";
 details=document.getElementById("details");
-number=9865876141
 let td=document.createElement("td");//1st td
 let td2=document.createElement("td");
 let td3=document.createElement("td");
 details.append(td,td2,td3)
-
 // get number
+function Pnumber(){
+  let phno=document.getElementById("PhNo").value;
+  return phno
+}
 async function mnumber(link){
     let result=await fetch(link);
     let response=await result.json();
@@ -15,8 +17,11 @@ async function mnumber(link){
   if(response.result!==true){
     root.innerText="enter crt number";
   }})()
-  td.innerText=response
-    
+  td.innerText=" "+1//response
+  
 }
+let number=Pnumber();
 url=`http://apilayer.net/api/validate?access_key=7216ec1b6be70844baee2c3087dafe68&number=${number}&country_code =IN&format=1`
-mnumber(url)
+console.log(number)
+mnumber(url);
+
