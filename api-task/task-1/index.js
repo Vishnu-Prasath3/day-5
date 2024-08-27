838c9715b42fc8d571de204ceb95ac4a73bf543b
VishnuPrasath <eeevishnuprasath@gmail.com>
2024-08-27 16:06:45 +0530

    commit added

--- api-task/task-1/index.js
+++ api-task/task-1/index.js
@@ -4,18 +4,20 @@ let root=document.getElementById("head-id");
 root.innerText="Enter An Number";
 
 details=document.getElementById("details");
-let td=document.createElement("td");//1st td
+let td =document.createElement("td");//1st td
 let td2=document.createElement("td");
 let td3=document.createElement("td");
 let td4=document.createElement("td");
 details.append(td,td2,td3,td4);
-td.innerText="-";
-td2.innerText="-";
-td3.innerText="-";
-td4.innerText="-";
+// - for initial value
+    td.innerText="-";
+    td2.innerText="-";
+    td3.innerText="-";
+    td4.innerText="-";
 // get number
 function Pnumber(){
   let phno=document.getElementById("PhNo").value;
+  // check wheather num or null
   if(document.getElementById("PhNo").value==""||document.getElementById("PhNo").value==undefined){
     console.log("enter an number");
   }
@@ -27,6 +29,8 @@ function Pnumber(){
   }
 }
 async function mnumber(num){
+  // try main task
+  try {
   let link=`http://apilayer.net/api/validate?access_key=7216ec1b6be70844baee2c3087dafe68&number=${num}&country_code =IN&format=1`
     let result=await fetch(link);
     let response=await result.json();
@@ -37,7 +41,11 @@ async function mnumber(num){
   if(response.success==false){
   
     td.innerText="unavailable";//response
-  }
+}
   td3.innerText=num;
-  
+}
+// to catch the error
+catch (error) {
+ console.log(error);   
+}
 }
