00155520b8ae32cc0fa5dfc05835843dc20413b6
vishnuprasath03 <eeevishnuprasath@gmail.com>
2024-08-27 14:12:17 +0530

    css added

--- api-task/task-1/index.js
+++ api-task/task-1/index.js
@@ -1,27 +1,43 @@
 let url,details; //api url
-let root=document.getElementById("root").innerText="enter an mobile number";
+
+let root=document.getElementById("head-id");
+root.innerText="Enter An Number";
+
 details=document.getElementById("details");
 let td=document.createElement("td");//1st td
 let td2=document.createElement("td");
 let td3=document.createElement("td");
-details.append(td,td2,td3)
+let td4=document.createElement("td");
+details.append(td,td2,td3,td4);
+td.innerText="-";
+td2.innerText="-";
+td3.innerText="-";
+td4.innerText="-";
 // get number
 function Pnumber(){
   let phno=document.getElementById("PhNo").value;
-  return phno
+  if(document.getElementById("PhNo").value==""||document.getElementById("PhNo").value==undefined){
+    console.log("enter an number");
+  }
+  else if(phno===isNaN){
+  td3.innerText="not an number";
+  }
+  else{
+    mnumber(phno);
+  }
 }
-async function mnumber(link){
+async function mnumber(num){
+  let link=`http://apilayer.net/api/validate?access_key=7216ec1b6be70844baee2c3087dafe68&number=${num}&country_code =IN&format=1`
     let result=await fetch(link);
     let response=await result.json();
     (()=>{
   if(response.result!==true){
     root.innerText="enter crt number";
   }})()
-  td.innerText=" "+1//response
+  if(response.success==false){
+  
+    td.innerText="unavailable";//response
+  }
+  td3.innerText=num;
   
 }
-let number=Pnumber();
-url=`http://apilayer.net/api/validate?access_key=7216ec1b6be70844baee2c3087dafe68&number=${number}&country_code =IN&format=1`
-console.log(number)
-mnumber(url);
-
